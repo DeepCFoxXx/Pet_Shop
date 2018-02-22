86788f1819a9244c31235025858051484e42493f
DeepCFoxXx <learncode23@gmail.com>
2018-02-22 12:56:49 +0000

    First Commit

---
+++ pet_shop_spec.rb
@@ -0,0 +1,76 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../pet_shop.rb'
+
+class TestPetShop < Minitest::Test
+
+  def setup
+
+    @customers = [
+      {
+        name: "Luke",
+        pets: [],
+        cash: 1000
+      },
+      {
+        name: "Liea",
+        pets: [],
+        cash: 1500
+      }
+    ]
+
+    @new_pet = {
+      name: "Ozzy"
+      pet_type: :cat,
+      breed: "Bengal",
+      price: 100
+    }
+
+    @pet_shop = {
+      pets: [
+        {
+          name: "Freddie",
+          pet_type: :cat,
+          breed: "British Shorthair",
+          price: 100,
+        },
+        {
+          name: "Robert",
+          pet_type: :cat,
+          breed: "Persian",
+          price: 200,
+        },
+        {
+          name: "Elton",
+          pet_type: :cat,
+          breed: "Ragdoll",
+          price: 300,
+        },
+        {
+          name: "Stevie",
+          pet_type: :cat,
+          breed: "Maine Coon",
+          price: 400,
+        },
+        {
+          name: "Stacy",
+          pet_type: :cat,
+          breed: "Russian Blue",
+          price: 500,
+        }
+      ],
+      admin: {
+        total_cash: 1000,
+        pets_sold: 0,
+      },
+      name: "Cats in Space"
+    }
+
+  end
+
+  # def test_pet_shop_name
+  #   name = pet_shop_name(@pet_shop)
+  #   assert_equal("Cats in Space", name)
+  # end
+
+end
